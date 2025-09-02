a5d6d9dc961d9b8d971de715b9ddf7498c74c21a
João Marcos <jobueno1230@gmail.com>
2025-09-01 21:30:36 -0300

    Feita a questão 1.

---
+++ lista1ex1.c
@@ -0,0 +1,20 @@
+#include "tab.h"
+#include <stdio.h>
+
+TAB* TAB_copia(TAB*a) {
+    if (!a) return NULL;
+    return TAB_cria(a->info, TAB_copia(a->esq), TAB_copia(a->dir));
+}
+
+int main(void) {
+    int vet[] = {4, 9, 8, 2, 1, 10, 14, 15,16, 19, 30, 20, 25, 9};
+    TAB* a = v2ab(vet, 14);
+    TAB_imp_pre(a);
+    printf("\n-----------------------------------------\n");
+    TAB* copia = TAB_copia(a);
+    TAB_imp_pre(copia);
+    printf("\n");
+    TAB_libera(copia);
+    TAB_libera(a);
+    return 0;
+}
