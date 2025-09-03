fcf80214ac2da35b51f163450e1b2aca567fe234
João Marcos <jobueno1230@gmail.com>
2025-09-02 21:42:47 -0300

    Feita questão 8 lista 1.

---
+++ lista1/lista1ex8.c
@@ -0,0 +1,20 @@
+#include "../tab/tab.h"
+#include <stdio.h>
+
+int ni(TAB*a) {
+    if (!a) return 0;
+    if ((a->esq)||(a->dir)) {
+        return 1 + ni(a->esq) + ni(a->dir);
+    }
+    return 0;
+}
+
+int main(void) {
+    int vet[] = {4, 9, 8, 2, 1, 10, 14, 18,16, 19, 30, 20, 25, 9, 23, 24, 25};
+    TAB* a = v2ab(vet, 17);
+    TAB_imp_ident(a);
+    printf("=-=-==-=-=-==-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-===-=-=-=-=-==-=-=-=-=-=\n");
+    printf("Qtd nos internos: %d\n", ni(a));
+    TAB_libera(a);
+    return 0;
+}
